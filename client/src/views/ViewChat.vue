14554fd4411f71015861ddf22c5583ffba4300da
Sofian Slimani <70762272+Sev7n2507@users.noreply.github.com>
2022-01-07 12:03:15 +0100

    add logout & scrollTop for messages

--- client/src/views/ViewChat.vue
+++ client/src/views/ViewChat.vue
@@ -1,5 +1,7 @@
 <template>
   <div class="messagerie">
+    <button Type="Button" @click="delStorage()">Logout</button>
+
     <Chat />
   </div>
 </template>
@@ -14,5 +16,26 @@ export default {
   components: {
     Chat,
   },
+
+  methods: {
+    delStorage() {
+      localStorage.removeItem('node_name');
+      localStorage.removeItem('node_token');
+      this.$router.push('/');
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  padding: 10px 30px;
+  border-radius: 20px;
+  border: 0px;
+  color: white;
+  background-color: navy;
+}
+button:hover {
+  cursor: pointer;
+}
+</style>
