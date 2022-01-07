0b1d790b39ef6b3652cabbe18e115337842047f2
Warsamé <wfarahabdi@gmail.com>
2022-01-07 02:47:55 +0100

    socket works

--- client/src/components/chat.vue
+++ client/src/components/chat.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <h1>Messagerie</h1>
-        <ul>
+        <ul id="msg-container">
             <li v-for="(message, index) in messages" :key="message._id + index" class="messages">
                 <div>{{message.content}}</div>
                 <div>{{message.username}}</div>
@@ -16,17 +16,22 @@
 
 <script>
 import axios from 'axios';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3000');
 
 export default {
     methods: {
         sendMessage() {
             let content = document.getElementById('input').value;
+
             axios.post(this.url_env +'/api/messages/create', {
                 content: content,
                 username: this.username
             }).then(function(response) {
                 console.log(response);
                 document.getElementById('input').value = '';
+                socket.emit('chatMessage')
             }).catch(function (error) {
                 console.log(error.response.data);
             });
@@ -38,19 +43,37 @@ export default {
             }).catch(error => {
                 console.log(error.response.data);
             });
+        },
+        insertMessage(data) {
+            const div = document.getElementById('msg-container');
+            const msg = document.querySelectorAll('.messages')[0];
+            let attr;
+            msg.getAttributeNames().forEach(att => {
+                if(att.startsWith('data')){
+                    attr = att
+                }
+            });
+            div.innerHTML += `
+            <li class="messages" ${attr}>
+                <div ${attr}>${data.content}</div>
+                <div ${attr}>${data.username}</div>
+            </li>`
         }
     },
     created() {
         if (localStorage.getItem('node_name') !== null) {
             this.username = localStorage.getItem('node_name');
             this.getMessages();
+            socket.on('insertMessage', data => {
+                this.insertMessage(data);
+            })
         } else {
             this.$router.push('/');
         }
-       
     },
     data() {
         return {
+            lastMessage: '',
             messages: '',
             url_env: 'http://localhost:4000',
             username: 'Test1',
